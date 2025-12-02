adbb41a4a0ee550d1ac69de17cdf870126bc01b1
niwath <138000745+misoniwath@users.noreply.github.com>
2025-12-02 00:08:37 -0500

    fix: script with different element input

--- element.sh
+++ element.sh
@@ -2,7 +2,7 @@
 
 PSQL="psql -U freecodecamp -d periodic_table -t -A -F '|' -c"
 
-# If no argument is provided
+# Check for argument
 if [[ -z $1 ]]; then
   echo "Please provide an element as an argument."
   exit 0
@@ -17,29 +17,23 @@ JOIN properties p ON e.atomic_number = p.atomic_number
 JOIN types t ON p.type_id = t.type_id
 WHERE e.atomic_number::text = '$ARG' OR e.symbol ILIKE '$ARG' OR e.name ILIKE '$ARG';")
 
-# If element not found
+# Check if element exists
 if [[ -z $RESULT ]]; then
   echo "I could not find that element in the database."
   exit 0
 fi
 
-# Remove any surrounding whitespace or quotes
-RESULT=$(echo $RESULT | tr -d "'\"")
-
-# Split result into variables
+# Split into variables
 IFS='|' read -r ATOMIC_NUMBER NAME SYMBOL TYPE MASS MELT BOIL <<< "$RESULT"
 
-# Capitalize first letter of symbol only
-SYMBOL="$(tr '[:lower:]' '[:upper:]' <<< ${SYMBOL:0:1})${SYMBOL:1}"
-
-# Remove trailing zeros from numeric values
-trim_trailing() {
-  printf "%s" "$1" | sed -E 's/\.?0+$//'
-}
-
-MASS=$(trim_trailing "$MASS")
-MELT=$(trim_trailing "$MELT")
-BOIL=$(trim_trailing "$BOIL")
+# Remove any single quotes from each variable
+ATOMIC_NUMBER=${ATOMIC_NUMBER//\'/}
+NAME=${NAME//\'/}
+SYMBOL=${SYMBOL//\'/}
+TYPE=${TYPE//\'/}
+MASS=${MASS//\'/}
+MELT=${MELT//\'/}
+BOIL=${BOIL//\'/}
 
 # Print exactly in FCC format
 echo "The element with atomic number $ATOMIC_NUMBER is $NAME ($SYMBOL). It's a $TYPE, with a mass of $MASS amu. $NAME has a melting point of $MELT celsius and a boiling point of $BOIL celsius."
